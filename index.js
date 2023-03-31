1cedeb02dfd6de4f1ed405f0955f504b36dc50a4
Kelvin <kelvinkimaru@gmail.com>
2023-03-31 09:24:29 +0300

    Completed task

---
+++ index.js
@@ -0,0 +1,9 @@
+//
+const divide = () => 2000/100;
+console.log(divide());
+//
+const square = parameter => parameter*parameter;
+console.log(square(2));
+//
+const add = (a,b) => a+b;
+console.log(add(3,4));
